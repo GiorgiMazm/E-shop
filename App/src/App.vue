3356b8008971152abbfadcb6082844fe92320713
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-03-29 11:08:55 +0200

    Initial commit

---
+++ App/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MyComp from "./components/MyComp.vue";
+
+const greeting = "greeting traveler";
+</script>
+
+<template>
+  <div>
+    my E-Shop
+    <MyComp :msg="greeting" />
+  </div>
+</template>
+
+<style scoped></style>
