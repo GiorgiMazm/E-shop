a99df1ac246a28b7e1912ce3dab164d3f6c0bef0
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-04-01 20:58:58 +0200

    Save products in Array

--- App/src/App.vue
+++ App/src/App.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
 import ProductItem from "./components/ProductItem.vue";
+import { Ref, ref } from "vue";
+
+interface Item {
+  name: string;
+  price: number;
+  link: string;
+  id: number;
+}
+
+const productsList: Ref<Item[]> = ref([
+  {
+    name: "Macbook",
+    price: 2000,
+    link: "https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021.jpg.og.jpg?202303230456",
+    id: 1,
+  },
+  {
+    name: "Macbook pro",
+    price: 2500,
+    link: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_EMEA_LANG_DE?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664497359389",
+    id: 2,
+  },
+
+  {
+    name: "Macbook air",
+    price: 1500,
+    link: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-macbook-air-gold-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145607000",
+    id: 3,
+  },
+
+  {
+    name: "Lenovo thinkpad",
+    price: 3000,
+    link: "https://laptopmedia.com/wp-content/uploads/2021/06/3-52-e1624453916559.jpg",
+    id: 4,
+  },
+]);
 </script>
 
 <template>
@@ -11,19 +48,11 @@ import ProductItem from "./components/ProductItem.vue";
         <h1 class="text-4xl text-center pt-5">Our product</h1>
         <div class="flex flex-wrap justify-around mt-5">
           <ProductItem
-            item-name="Macbook"
-            price="2000"
-            img-link="https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021.jpg.og.jpg?202303230456"
-          />
-          <ProductItem
-            item-name="Macbook pro"
-            price="3000"
-            img-link="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_EMEA_LANG_DE?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664497359389"
-          />
-          <ProductItem
-            item-name="Macbook air"
-            price="1500"
-            img-link="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-macbook-air-gold-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145607000"
+            v-for="item in productsList"
+            :item-name="item.name"
+            :price="item.price"
+            :img-link="item.link"
+            :key="item.id"
           />
         </div>
       </div>
