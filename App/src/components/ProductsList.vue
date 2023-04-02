0039d40760da855f85ae1ead2ba5ec30a50f1a6a
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-04-02 09:47:00 +0200

    ProductsList component

---
+++ App/src/components/ProductsList.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref, Ref } from "vue";
+import ProductItem from "./ProductItem.vue";
+
+interface Item {
+  name: string;
+  price: number;
+  link: string;
+  id: number;
+}
+
+const productsList: Ref<Item[]> = ref([
+  {
+    name: "Macbook",
+    price: 2000,
+    link: "https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021.jpg.og.jpg?202303230456",
+    id: 1,
+  },
+  {
+    name: "Macbook pro",
+    price: 2500,
+    link: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206_GEO_EMEA_LANG_DE?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664497359389",
+    id: 2,
+  },
+
+  {
+    name: "Macbook air",
+    price: 1500,
+    link: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-macbook-air-gold-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145607000",
+    id: 3,
+  },
+
+  {
+    name: "Lenovo thinkpad",
+    price: 3000,
+    link: "https://laptopmedia.com/wp-content/uploads/2021/06/3-52-e1624453916559.jpg",
+    id: 4,
+  },
+]);
+</script>
+
+<template>
+  <section class="bg-gray-500 text-gray-200 pb-7">
+    <div class="container mx-auto">
+      <h1 class="text-4xl text-center pt-5">Our product</h1>
+      <div class="flex flex-wrap justify-around mt-5">
+        <ProductItem
+          v-for="item in productsList"
+          :item-name="item.name"
+          :price="item.price"
+          :img-link="item.link"
+          :key="item.id"
+        />
+      </div>
+    </div>
+  </section>
+</template>
