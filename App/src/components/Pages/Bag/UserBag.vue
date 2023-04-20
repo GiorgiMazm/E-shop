9b40615316efc9ffff17df93e2ba65ee868fb808
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-04-20 17:56:58 +0200

    add item amount

--- App/src/components/Pages/Bag/UserBag.vue
+++ App/src/components/Pages/Bag/UserBag.vue
@@ -7,12 +7,14 @@ const store = useProductStore();
 const userBag = computed(() => {
   return store.userModule.getCurrentUser?.bag;
 });
+
+const itemAmount = computed(() => store.userModule.getCurrentUser?.bag.length);
 </script>
 
 <template>
   <section class="bg-gray-400">
     <div class="container mx-auto">
-      <h2 class="text-2xl">Your items:</h2>
+      <h2 class="text-2xl">Your items: {{ itemAmount }}</h2>
       <ul class="flex flex-wrap justify-center">
         <UserBagItem
           v-for="(item, index) in userBag"
