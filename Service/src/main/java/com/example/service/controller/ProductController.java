b1c631968898174c406b430140f8fad6d99c3478
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-04-22 21:54:50 +0200

    set up controller for Product

--- Service/src/main/java/com/example/service/controller/ProductController.java
+++ Service/src/main/java/com/example/service/controller/ProductController.java
@@ -2,9 +2,7 @@ package com.example.service.controller;
 
 import com.example.service.model.Product;
 import com.example.service.repository.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +18,31 @@ public class ProductController {
     }
 
     @GetMapping(path = "/productList")
-    public List<Product> home() {
+    public List<Product> getAllProduct() {
         return this.productRepository.findAll();
+    }
+
+    @PostMapping("newProduct")
+    public void createProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }
+
+    @DeleteMapping(path = "deleteProduct/{productId}")
+    public void deleteProduct(@PathVariable("productId") Long productId) {
+        productRepository.deleteById(productId);
+    }
+
+    @PutMapping(path = "editProduct/{productId}")
+    public void updateProduct(@PathVariable(value = "productId") Long productId, @RequestBody Product newProduct) {
+        Product product = productRepository.findById(productId).get();
+
+        product.setName(newProduct.getName());
+        product.setCategory(newProduct.getCategory());
+        product.setLink(newProduct.getLink());
+        product.setPrice(newProduct.getPrice());
+        product.setDescription(newProduct.getDescription());
+
+        System.out.println(product);
+        productRepository.save(product);
     }
 }
