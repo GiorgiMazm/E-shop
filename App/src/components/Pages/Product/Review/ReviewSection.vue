0c6874e3031def07727607c4dea2531c45e09473
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2023-05-06 20:01:14 +0200

    validation for create review

--- App/src/components/Pages/Product/Review/ReviewSection.vue
+++ App/src/components/Pages/Product/Review/ReviewSection.vue
@@ -1,22 +1,44 @@
 <script setup lang="ts">
 import ReviewItem from "./ReviewItem.vue";
-import { ref } from "vue";
+import { reactive } from "vue";
 import { useProductStore } from "../../../../stores/ProductStore";
 import Review from "../../../../types/Review";
 import Item from "../../../../types/Item";
+import { integer, minValue, required } from "@vuelidate/validators";
+import { useVuelidate } from "@vuelidate/core";
 
-const store = useProductStore();
-const title = ref("");
-const description = ref("");
-const rate = ref(10);
 const props = defineProps<{
   item: Item;
 }>();
+const store = useProductStore();
+const formData = reactive({
+  title: "",
+  description: "",
+  rate: 10,
+});
 
+const rules = {
+  title: { required },
+  description: { required },
+  rate: { required, integer, minValue: minValue(1) },
+};
+
+const validation = useVuelidate(rules, formData);
 function clearInputs() {
-  title.value = "";
-  description.value = "";
-  rate.value = 10;
+  formData.title = "";
+  formData.description = "";
+  formData.rate = 10;
+}
+function createReview() {
+  if (validation.value.$invalid) return;
+  store.reviewModule.addReview({
+    product: props.item,
+    user: store.userModule.getCurrentUser,
+    title: formData.title,
+    description: formData.description,
+    rate: formData.rate,
+  } as Review);
+  clearInputs();
 }
 </script>
 
@@ -32,44 +54,44 @@ function clearInputs() {
     <form
       class="mt-4"
       v-if="store.userModule.getCurrentUser"
-      @submit.prevent="
-        store.reviewModule.addReview({
-          product: props.item,
-          user: store.userModule.getCurrentUser,
-          title: title,
-          description: description,
-          rate: rate,
-        } as Review);
-        clearInputs();
-      "
+      @submit.prevent="createReview"
     >
       <div class="flex flex-col">
         <label>Review title</label>
         <input
           class="my-2 text-black p-2 bg-gray-200 border-2 border-gray-600"
           placeholder="Title..."
-          v-model="title"
+          v-model="formData.title"
         />
+        <span class="text-red-600 pb-3" v-if="validation.title.$invalid"
+          >Title is required</span
+        >
       </div>
       <div class="flex flex-col">
         <label>Add your review</label>
         <textarea
           class="my-2 text-black p-2 bg-gray-200 border-2 border-gray-600"
           placeholder="Tell about your experience with this product..."
-          v-model="description"
+          v-model="formData.description"
         />
+        <span class="text-red-600 pb-3" v-if="validation.description.$invalid"
+          >Description is required</span
+        >
       </div>
 
       <div class="flex flex-col">
         <label>Rate this product on scale 1 to 10</label>
         <select
           class="my-2 text-black p-2 bg-gray-200 border-2 border-gray-600"
-          v-model="rate"
+          v-model="formData.rate"
         >
           <option v-for="num in 10" :key="num" :value="num">
             {{ num }}
           </option>
         </select>
+        <span class="text-red-600 pb-3" v-if="validation.rate.$invalid"
+          >Rate is required</span
+        >
       </div>
 
       <button
